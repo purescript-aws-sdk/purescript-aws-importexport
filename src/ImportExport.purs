548d181ec9d9d0031633463a5682470a53823cd0
Philippe Vinchon <pvinchon@gilt.com>
2018-03-12 18:00:47 +0000

    PureScript mapping for aws-sdk-js 2.207.0

--- src/ImportExport.purs
+++ src/ImportExport.purs
@@ -20,37 +20,47 @@ import Data.StrMap as StrMap
 import AWS.Request as Request
 import AWS.Request.Types as Types
 
-serviceName = "ImportExport" :: String
-
 
 -- | This operation cancels a specified job. Only the job owner can cancel it. The operation fails if the job has already started or is complete.
 cancelJob :: forall eff. CancelJobInput -> Aff (exception :: EXCEPTION | eff) CancelJobOutput
-cancelJob = Request.request serviceName "cancelJob" 
+cancelJob = Request.request service method  where
+    service = Request.ServiceName "ImportExport"
+    method = Request.MethodName "cancelJob"
 
 
 -- | This operation initiates the process of scheduling an upload or download of your data. You include in the request a manifest that describes the data transfer specifics. The response to the request includes a job ID, which you can use in other operations, a signature that you use to identify your storage device, and the address where you should ship your storage device.
 createJob :: forall eff. CreateJobInput -> Aff (exception :: EXCEPTION | eff) CreateJobOutput
-createJob = Request.request serviceName "createJob" 
+createJob = Request.request service method  where
+    service = Request.ServiceName "ImportExport"
+    method = Request.MethodName "createJob"
 
 
 -- | This operation generates a pre-paid UPS shipping label that you will use to ship your device to AWS for processing.
 getShippingLabel :: forall eff. GetShippingLabelInput -> Aff (exception :: EXCEPTION | eff) GetShippingLabelOutput
-getShippingLabel = Request.request serviceName "getShippingLabel" 
+getShippingLabel = Request.request service method  where
+    service = Request.ServiceName "ImportExport"
+    method = Request.MethodName "getShippingLabel"
 
 
 -- | This operation returns information about a job, including where the job is in the processing pipeline, the status of the results, and the signature value associated with the job. You can only return information about jobs you own.
 getStatus :: forall eff. GetStatusInput -> Aff (exception :: EXCEPTION | eff) GetStatusOutput
-getStatus = Request.request serviceName "getStatus" 
+getStatus = Request.request service method  where
+    service = Request.ServiceName "ImportExport"
+    method = Request.MethodName "getStatus"
 
 
 -- | This operation returns the jobs associated with the requester. AWS Import/Export lists the jobs in reverse chronological order based on the date of creation. For example if Job Test1 was created 2009Dec30 and Test2 was created 2010Feb05, the ListJobs operation would return Test2 followed by Test1.
 listJobs :: forall eff. ListJobsInput -> Aff (exception :: EXCEPTION | eff) ListJobsOutput
-listJobs = Request.request serviceName "listJobs" 
+listJobs = Request.request service method  where
+    service = Request.ServiceName "ImportExport"
+    method = Request.MethodName "listJobs"
 
 
 -- | You use this operation to change the parameters specified in the original manifest file by supplying a new manifest file. The manifest file attached to this request replaces the original manifest file. You can only use the operation after a CreateJob request but before the data transfer starts and you can only use it on jobs you own.
 updateJob :: forall eff. UpdateJobInput -> Aff (exception :: EXCEPTION | eff) UpdateJobOutput
-updateJob = Request.request serviceName "updateJob" 
+updateJob = Request.request service method  where
+    service = Request.ServiceName "ImportExport"
+    method = Request.MethodName "updateJob"
 
 
 -- | Specifies the version of the client tool.
